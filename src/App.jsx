6c0d8719f7f1ee846d1774729cb21309f83f37f4
SK02K1 <ksourabh458@gmail.com>
2022-03-16 13:28:10 +0530

    refactor: remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Navbar, Wrapper } from './components/index';
 import { Home } from './pages/Home/Home';
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { makeServer } from './server';
 
 // Call make Server
 makeServer();
@@ -11,5 +10,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );

8d222a7dbb417c38f1621d1ff32911140ed28804
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-04-07 15:53:32 +0530

    feat: scroll to top when page is mounted

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,5 +1,11 @@
 import './Wrapper.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return <div className='wrapper'>{children}</div>;
 };
